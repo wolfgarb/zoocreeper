7f7889a57975920716102ab35e6c9974c581b5c7
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-11-03 09:54:14 -0400

    add heroku

---
+++ server.js
@@ -0,0 +1,60 @@
+const { query } = require("express");
+const express = require("express");
+const app = express();
+
+const { animals } = require("./data/animals");
+
+function filterByQuery(query, animalsArray) {
+  let persTraitsArr = [];
+  let filteredResults = animalsArray;
+  if (query.personalityTraits) {
+    // save ptraits as a dedicated array
+    // if ptraits is a string, place it into a new array and save.
+    if (typeof query.personalityTraits === 'string') {
+      persTraitsArr = [query.personalityTraits];
+    } else {
+      persTraitsArr = query.personalityTraits;
+    }
+    // loop through each trait in array
+    persTraitsArr.forEach(trait => {
+      // Check the trait against each animal in the filteredResults array.
+      // Remember, it is initially a copy of the animalsArray,
+      // but here we're updating it for each trait in the .forEach() loop.
+      // For each trait being targeted by the filter, the filteredResults
+      // array will then contain only the entries that contain the trait,
+      // so at the end we'll have an array of animals that have every one 
+      // of the traits when the .forEach() loop is finished.
+      filteredResults = filteredResults.filter(
+        animal => animal.personalityTraits.indexOf(trait) !== -1
+      );
+    });
+  }
+  if (query.diet) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.diet === query.diet
+    );
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.species === query.species
+    );
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.name === query.name
+    );
+  }
+  return filteredResults;
+}
+
+app.get("/api/animals", (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }  
+  res.json(results);
+});
+
+app.listen(3001, () => {
+  console.log(`API server now on port 3001!`);
+});
