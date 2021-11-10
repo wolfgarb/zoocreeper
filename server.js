b1be8c121be02ef0c3f9d5404b85b1940140df19
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-11-10 17:04:27 -0500

    test

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 
 function filterByQuery(query, animalsArray) {
@@ -116,7 +117,22 @@ app.post('/api/animals', (req, res) => {
     const animal = newAnimal(req.body, animals);
     res.json(animal);
   }
+});
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
